81fd693d79fde141e93b012ad4772247db3aea3a
krojerLukas <lukas.krojer@gmx.de>
2024-12-14 12:12:57 +0100

    serach bar visible in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import SearchBar from "./components/SearchBar.jsx";
 
+// styles
+import './assets/app.css';
+
 function App() {
+    function handleInput () {
+
+    }
 
     return (
-        <div>
-            <SearchBar/>
+        <div className="search-bar movie-search-bar">
+            <SearchBar input={handleInput}/>
         </div>
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,18 @@
-function SearchBar () {
+import {useState} from "react";
 
+function SearchBar() {
+    const [userInput, setUserInput] = useState("");
+
+    function handleChange(e) {
+        // if data is backspace, enter
+        if (e.nativeEvent.data == null) return;
+
+        setUserInput(userInput + e.nativeEvent.data);
+    }
+
+    return (
+        <input onChange={handleChange}></input>
+    )
 }
 
 export default SearchBar
